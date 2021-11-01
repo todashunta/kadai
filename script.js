62f00857cc4c5587e04702afc36c4b5cd8a76e58
todashunta <todashuntastem@gmail.com>
2021-11-01 18:25:48 +0900

    11/1 業後

--- script.js
+++ script.js
@@ -7,14 +7,39 @@ window.addEventListener('scroll', () => {
   for (let i = 0; i < fadeInTarget.length; i++){
     const rectTop = fadeInTarget[i].getBoundingClientRect().top;
     const rectBottom = fadeInTarget[i].getBoundingClientRect().bottom;
-    if (rectTop + 200 > 800 || rectBottom - 200 > 0) {
+    const viewHeight = document.documentElement.clientHeight;
+    if (rectTop < viewHeight * 0.8 || rectBottom > 100) {
       fadeInTarget[i].classList.add('scroll-in');
     }
-    if(rectBottom - 200 < 0 || rectTop + 200 > 800){
+    if(rectTop < viewHeight * 0.2 || rectBottom < 100){
       fadeInTarget[i].classList.remove('scroll-in');
+    }
+    // const rect = fadeInTarget[i].getBoundingClientRect().top;
+    // const scroll = window.pageYOffset || document.documentElement.scrollTop;
+    // const offset = rect + scroll;
+    // const windowHight = window.innerHeight;
+    if(i == 0){
+      console.log({
+        'rect': Math.floor(rectBottom),
+        'view': Math.floor(viewHeight),
+        'if': rectBottom > 100
+      });
     }
   }
 });
+
+// let fadeInTarget = document.querySelectorAll('.fade-in');
+// window.addEventListener('scroll', () => {
+//   for (let i = 0; i < fadeInTarget.length; i++){
+//     const rect = fadeInTarget[i].getBoundingClientRect().top;
+//     const scroll = window.pageYOffset || document.documentElement.scrollTop;
+//     const offset = rect + scroll;
+//     const windowHeight = window.innerHeight; // 現在のブラウザの高さ
+//     if (scroll > offset - windowHeight + 150) {
+//       fadeInTarget[i].classList.add('scroll-in');
+//     }
+//   }
+// });
 
 const sliderItems = document.getElementsByClassName('slider-item');
 let sliderCount = 0;
